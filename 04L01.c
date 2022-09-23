dfef3308eb5f64a6ed538d6a1008dc5679e8d634
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-23 14:52:13 +0100

    print character

---
+++ 04L01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print characters to screen.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c1;
+	char c2;
+
+	c1 = 'A';
+	c2 = 'a';
+
+	printf("Convert the value of c1 to character: %c.\n", c1);
+	printf("Convert the value of c2 to character: %c.\n", c2);
+
+	return (0);
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+--**This is my learning journey**--
+--I am learning how to program in C.--
+* Print character in screen.
