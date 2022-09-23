92706a3b6e246e26db787640028957870e8d54ce
Felix-code <84644592+Felix-code@users.noreply.github.com>
2022-09-23 15:25:22 +0100

    print characters

--- 04L01.c
+++ 04L01.c
@@ -7,11 +7,7 @@
 
 int main(void)
 {
-	char c1;
-	char c2;
-
-	c1 = 'A';
-	c2 = 'a';
+	char c1 = 'A', c2 = 'a';
 
 	printf("Convert the value of c1 to character: %c.\n", c1);
 	printf("Convert the value of c2 to character: %c.\n", c2);
---
+++ 04L02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print characters to screen.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char c1 = 65, c2 = 97;
+
+        printf("The character that has the numeric value of 65 is: %c.\n", c1);
+        printf("The character that has the nummeric value of 97 is: %c.\n", c2);
+
+        return (0);
+
+}
+
